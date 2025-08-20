7537df07e7aaf7c6c7290e3a59b163e0b953cb25
mubu002 <mustafa_buga1@hotmail.com>
2025-08-20 13:48:45 +0200

    opprettet test og funksjon romanNumerals

---
+++ romanNumerals.test.js
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+
+function romanNumerals(number) {
+  return undefined;
+}
+
+test("1 in roman numerals is I", () => {
+  expect(romanNumerals(1)).toBe("I");
+});
