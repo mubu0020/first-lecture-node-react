79f5ed477f069ede6a73e99b080179027eaf0157
Thea <thgy001@student.kristiania.no>
2025-08-20 13:54:58 +0200

    ny test lagt til

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,9 +1,13 @@
 import { test, expect } from "vitest";
 
 function romanNumerals(number) {
-  return undefined;
+  return "I";
 }
 
 test("1 in roman numerals is I", () => {
   expect(romanNumerals(1)).toBe("I");
+});
+
+test("2 in roman numerals is II", () => {
+  expect(romanNumerals(2)).toBe("II");
 });
